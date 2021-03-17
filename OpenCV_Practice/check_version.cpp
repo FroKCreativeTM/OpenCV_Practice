313eb91da354b0278dc6865b186bf9b7b79f2a61
승철 차 <cscculture95@gmail.com>
2021-03-17 10:52:30 +0900

    초기 커밋.

---
+++ OpenCV_Practice/check_version.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Hello OpenCV : " << CV_VERSION << std::endl;
+
+	cv::Mat img;
+	img = cv::imread("lenna.bmp");
+
+	if (img.empty())
+	{
+		std::cerr << "Image Load fail" << std::endl;
+		return -1;
+	}
+
+	cv::namedWindow("lenna");
+	cv::imshow("lenna", img);
+
+	cv::waitKey();
+
+	return 0;
+}
